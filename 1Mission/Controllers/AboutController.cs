b9450a5844fae407342d6884c4d1e4f2991606ad
nostreim <nostreim@gmail.com>
2013-01-24 14:37:58 -0700

    Added Who We Are and Meet the Founder pages

---
+++ 1Mission/Controllers/AboutController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1Mission.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public ActionResult WhoWeAre()
+        {
+            return View();
+        }
+
+        public ActionResult MeetTheFounder()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
